3f08da4bd05a27488058381f2435f79cd8e68596
ericblg <ericferrari98@hotmail.com>
2025-10-04 13:55:36 -0300

    atualização menu e footer

--- script.js
+++ script.js
@@ -28,3 +28,44 @@ new Swiper('.card-wrapper', {
     }
   }
 });
+
+/* LOGIN */
+
+const abrirLogin = document.getElementById("abrirLogin");
+const fecharPopup = document.getElementById("fecharPopup");
+const overlay = document.getElementById("overlay");
+const loginPopup = document.getElementById("loginPopup");
+
+const loginForm = document.getElementById("loginForm");
+const cadastroForm = document.getElementById("cadastroForm");
+const mostrarCadastro = document.getElementById("mostrarCadastro");
+const mostrarLogin = document.getElementById("mostrarLogin");
+
+abrirLogin.addEventListener("click", (e) => {
+  e.preventDefault(); // impede redirecionamento
+  overlay.style.display = "block";
+  loginPopup.style.display = "block";
+  loginForm.style.display = "block";
+  cadastroForm.style.display = "none";
+});
+
+fecharPopup.addEventListener("click", fecharTudo);
+overlay.addEventListener("click", fecharTudo);
+
+function fecharTudo() {
+  overlay.style.display = "none";
+  loginPopup.style.display = "none";
+}
+
+// Alternar Login <-> Cadastro
+mostrarCadastro.addEventListener("click", (e) => {
+  e.preventDefault();
+  loginForm.style.display = "none";
+  cadastroForm.style.display = "block";
+});
+
+mostrarLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  cadastroForm.style.display = "none";
+  loginForm.style.display = "block";
+});
